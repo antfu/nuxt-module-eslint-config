48f504df4765be1c05a5fe54ab5d4e73b06f38d0
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-19 19:01:42 +0100

    feat: add nuxt specific plugin
    Co-authored-by: DamianGlowala <DamianGlowala@users.noreply.github.com>

--- build.config.ts
+++ build.config.ts
@@ -4,6 +4,7 @@ export default defineBuildConfig({
   entries: [
     'src/module.ts',
     'src/config.ts',
+    'src/plugin.ts',
   ],
   rollup: {
     emitCJS: true,
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 import type { FlatConfig, NuxtESLintConfigOptions } from './types'
 import setup from './parts/setup'
 import disbales from './parts/disables'
+import nuxt from './parts/nuxt'
 
 export function createBasicNuxtConfig(options: NuxtESLintConfigOptions = {}): FlatConfig[] {
   const items: FlatConfig[] = []
@@ -8,6 +9,7 @@ export function createBasicNuxtConfig(options: NuxtESLintConfigOptions = {}): Fl
   if (options.features?.setup !== false)
     items.push(...setup())
 
+  items.push(...nuxt())
   items.push(...disbales(options))
 
   return items
